1736cc605091dd09f4eb638932dc43802ea97784
Matias Manuel Zambellini <matias.zambellini@mi.unc.edu.ar>
2026-06-04 12:29:28 -0300

    agregamos archivos nose que hacen

---
+++ Debug/src/Trabajo_practico_final.d
@@ -0,0 +1,32 @@
+src/Trabajo_practico_final.o src/Trabajo_practico_final.d: \
+ ../src/Trabajo_practico_final.c \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/LPC17xx.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/core_cm3.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/core_cmInstr.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/core_cmFunc.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/lpc17xx.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_adc.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/LPC17xx_timer.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_uart.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpdma.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h \
+ C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_systick.h
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/LPC17xx.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/core_cm3.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/core_cmInstr.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/core_cmFunc.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/system_LPC17xx.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpio.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc_types.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_pinsel.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\inc/lpc17xx.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_adc.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/LPC17xx_timer.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_uart.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_gpdma.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_dac.h:
+C:\Users\Windows\ 10\Documents\MCUXpressoIDE_25.6.136\workspace\CMSISv2p00_LPC17xx\Drivers\inc/lpc17xx_systick.h:
